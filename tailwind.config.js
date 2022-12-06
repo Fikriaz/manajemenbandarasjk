045043a5279b5ad832b63dd604fd046872268ba2
Octaviano <215232220@students.uii.ac.id>
2022-12-07 01:16:31 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '960px',
+      xl: '1280px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
